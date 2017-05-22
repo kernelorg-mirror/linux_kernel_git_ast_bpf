12e124c181497a13617ca12fb1a8bdc092aed414
Dave Watson <davejwatson@fb.com>
2017-05-22 10:09:51 -0700

    tcp: ULP infrastructure
    Add the infrustructure for attaching user level protocols over
    TCP sockets. Based on a similar infrastructure in tcp_cong.
    
    Change-Id: I094bd477e64f544ba7db09b1b7da94b362a9d806
    Signed-off-by: Boris Pismenny <borisp@mellanox.com>

--- include/net/inet_connection_sock.h
+++ include/net/inet_connection_sock.h
@@ -97,6 +97,8 @@ struct inet_connection_sock {
 	__u32			  icsk_pmtu_cookie;
 	const struct tcp_congestion_ops *icsk_ca_ops;
 	const struct inet_connection_sock_af_ops *icsk_af_ops;
+	const struct tcp_ulp_ops  *icsk_ulp_ops;
+	void			  *icsk_ulp_data;
 	unsigned int		  (*icsk_sync_mss)(struct sock *sk, u32 pmtu);
 	__u8			  icsk_ca_state:6,
 				  icsk_ca_setsockopt:1,
