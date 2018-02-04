10319e05a79d77e69d5764277333313823ffa728
Alexei Starovoitov <ast@kernel.org>
2018-02-03 18:39:20 -0800

    modules: allow modprobe load regular elf binaries
    As the first step in development of bpfilter project [1] the request_module()
    code is extended to allow user mode helpers to be invoked. Idea is that
    user mode helpers are built as part of the kernel build and installed as
    traditional kernel modules with .ko file extension into distro specified
    location, such that from a distribution point of view, they are no different
    than regular kernel modules. Thus, allow request_module() logic to load such
    user mode helper (umh) modules via:
    
      request_module("foo") ->
        call_umh("modprobe foo") ->
          sys_finit_module(FD of /lib/modules/.../foo.ko) ->
            call_umh(struct file)
    
    Such approach enables kernel to delegate functionality traditionally done
    by kernel modules into user space processes (either root or !root) and
    reduces security attack surface of such new code, meaning in case of
    potential bugs only the umh would crash but not the kernel. Another
    advantage coming with that would be that bpfilter.ko can be debugged and
    tested out of user space as well (e.g. opening the possibility to run
    all clang sanitizers, fuzzers or test suites for checking translation).
    Also, such architecture makes the kernel/user boundary very precise:
    control plane is done by the user space while data plane stays in the kernel.
    
    It's easy to distinguish "umh module" vs traditional kernel module:
    
    $ readelf -h bld/net/bpfilter/bpfilter.ko|grep Type
      Type:                              EXEC (Executable file)
    $ readelf -h bld/net/ipv4/netfilter/iptable_filter.ko |grep Type
      Type:                              REL (Relocatable file)
    
    Since umh can crash, can be oom-ed by the kernel, 'kill -9' by admin,
    the subsystem that uses them (like bpfilter) need to manage life
    time of umh on its own, so module infra doesn't do any accounting
    of them. They don't appear in "lsmod" and cannot be "rmmod".
    Multiple request_module("umh") will load multiple umh.ko processes.
    
    Similar to kernel modules the kernel will be tainted if "umh module"
    has invalid signature.
    
    [1] https://lwn.net/Articles/747551/
    
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- kernel/umh.c
+++ kernel/umh.c
@@ -97,9 +97,13 @@ static int call_usermodehelper_exec_async(void *data)
 
 	commit_creds(new);
 
-	retval = do_execve(getname_kernel(sub_info->path),
-			   (const char __user *const __user *)sub_info->argv,
-			   (const char __user *const __user *)sub_info->envp);
+	if (sub_info->file)
+		retval = do_execve_file(sub_info->file,
+					sub_info->argv, sub_info->envp);
+	else
+		retval = do_execve(getname_kernel(sub_info->path),
+				   (const char __user *const __user *)sub_info->argv,
+				   (const char __user *const __user *)sub_info->envp);
 out:
 	sub_info->retval = retval;
 	/*
@@ -392,6 +396,20 @@ struct subprocess_info *call_usermodehelper_setup(const char *path, char **argv,
 	return sub_info;
 }
 EXPORT_SYMBOL(call_usermodehelper_setup);
+
+struct subprocess_info *call_usermodehelper_setup_file(struct file *file)
+{
+	struct subprocess_info *sub_info;
+
+	sub_info = kzalloc(sizeof(struct subprocess_info), GFP_KERNEL);
+	if (!sub_info)
+		return NULL;
+
+	INIT_WORK(&sub_info->work, call_usermodehelper_exec_work);
+	sub_info->path = "/dev/null";
+	sub_info->file = file;
+	return sub_info;
+}
 
 /**
  * call_usermodehelper_exec - start a usermode application
