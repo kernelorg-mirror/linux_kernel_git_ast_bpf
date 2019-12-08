8cd62cfd187dfeabb695eb60d922aa7c0e5e257c
David Miller <davem@davemloft.net>
2019-12-07 16:04:22 -0800

    bpf: Remove preemption disable from simple call sites.
    All of these cases are strictly of the form:
    
    	preempt_disable();
    	BPF_PROG_RUN(...);
    	preempt_enable();
    
    BPF_PROG_RUN() is now a wrapper around __BPF_PROG_RUN()
    which disables migration (via RT local locking primitives
    or preemption disabling).
    
    Therefore, this is a trivially correct transformation.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20191207.160422.913393771404629217.davem@davemloft.net

--- net/core/flow_dissector.c
+++ net/core/flow_dissector.c
@@ -913,9 +913,7 @@ bool bpf_flow_dissect(struct bpf_prog *prog, struct bpf_flow_dissector *ctx,
 		     (int)FLOW_DISSECTOR_F_STOP_AT_ENCAP);
 	flow_keys->flags = flags;
 
-	preempt_disable();
 	result = BPF_PROG_RUN(prog, ctx);
-	preempt_enable();
 
 	flow_keys->nhoff = clamp_t(u16, flow_keys->nhoff, nhoff, hlen);
 	flow_keys->thoff = clamp_t(u16, flow_keys->thoff,
