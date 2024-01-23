bb754a8df3e207036a4713412cd05397f42d3e6f
Alexei Starovoitov <ast@kernel.org>
2024-01-22 18:58:56 -0800

    libbpf: Add support for bpf_arena.
    mmap() bpf_arena right after creation, since the kernel needs to
    remember the address returned from mmap. This is user_vm_start.
    The verifier will store upper 32-bit of it in every BPF_STX instruction
    that stores 'uptr' pointer into memory.
    
    Use traditional map->value_size * map->max_entries to calculate mmap sz,
    though it's not the best fit.
    
    Also don't set BTF at bpf_arena creation time, since it doesn't support it.
    
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- tools/lib/bpf/libbpf.c
+++ tools/lib/bpf/libbpf.c
@@ -4842,6 +4842,7 @@ static int bpf_object__create_map(struct bpf_object *obj, struct bpf_map *map, b
 	case BPF_MAP_TYPE_SOCKHASH:
 	case BPF_MAP_TYPE_QUEUE:
 	case BPF_MAP_TYPE_STACK:
+	case BPF_MAP_TYPE_ARENA:
 		create_attr.btf_fd = 0;
 		create_attr.btf_key_type_id = 0;
 		create_attr.btf_value_type_id = 0;
@@ -4897,6 +4898,21 @@ static int bpf_object__create_map(struct bpf_object *obj, struct bpf_map *map, b
 	/* obj->gen_loader case, prevent reuse_fd() from closing map_fd */
 	if (map->fd == map_fd)
 		return 0;
+
+	if (def->type == BPF_MAP_TYPE_ARENA) {
+		size_t mmap_sz;
+
+		mmap_sz = bpf_map_mmap_sz(def->value_size, def->max_entries);
+		map->mmaped = mmap(NULL, mmap_sz, PROT_READ | PROT_WRITE, MAP_SHARED,
+				   map_fd, 0);
+		if (map->mmaped == MAP_FAILED) {
+			err = -errno;
+			map->mmaped = NULL;
+			pr_warn("map '%s': failed to mmap bpf_arena: %d\n",
+				bpf_map__name(map), err);
+			return err;
+		}
+	}
 
 	/* Keep placeholder FD value but now point it to the BPF map object.
 	 * This way everything that relied on this map's FD (e.g., relocated
